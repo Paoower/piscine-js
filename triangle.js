b773d1e71753cb9390a5ddfeeed95177d024fa26
mtrebert <maxime.trebert@gmail.com>
2025-01-08 15:36:00 +0100

    triangle

---
+++ triangle.js
@@ -0,0 +1,8 @@
+// The function creates a string representing a triangle with the given character and height.
+function triangle(char,height){
+    let result = "";
+    for (let i = 1; i <= height; i++){
+        result += char.repeat(i) + (i === height ? "" : "\n");
+    }
+    return result;
+}
